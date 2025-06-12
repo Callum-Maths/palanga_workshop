1d8800a8ffd24050f3f97b03ba16eea578392728
Callum-Gateway <callum.marsh99@icloud.com>
2025-06-12 07:24:42 +0100

    misc

--- finding_trends.py
+++ finding_trends.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_parquet('part-00000-c0fa7496-921d-48fd-88e8-33ac5109cff7-c000.snappy.parquet')
+class FindingTrends():
 
-df_head = df.head(100)
-print(df_head)
-
-df_head.to_csv('customer_data_example.csv', index=False)
+    def __init__(self):
+        self.contract_data = pd.read_parquet('customer_contract_data.parquet') # 2 snapshots for each customer
+        usage_data1 = pd.read_parquet('customer_usage_data1.parquet') # usage of each customer
+        usage_data2 = pd.read_parquet('customer_usage_data2.parquet') # usage of each customer
+        usage_data3 = pd.read_parquet('customer_usage_data3.parquet') # usage of each customer
+        usage_data4 = pd.read_parquet('customer_usage_data4.parquet') # usage of each customer
+        self.usage_data = pd.concat([usage_data1, usage_data2, usage_data3, usage_data4], ignore_index=True)
+        self.vas_recommendation = pd.read_csv('vas_recommendation.csv')
 
